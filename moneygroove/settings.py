5f25f5b049adade0101417d5bf4fd103fc0b1a3f
Anders Hovmöller <anders.hovmoller@dryft.se>
2023-10-25 21:32:58 +0200

    .

--- moneygroove/settings.py
+++ moneygroove/settings.py
@@ -154,7 +154,8 @@ DATETIME_FORMAT = 'Y-m-d H:i:s'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = str(Path(BASE_DIR) / 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
